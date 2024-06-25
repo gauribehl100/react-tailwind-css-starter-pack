d1109bf7349fede6eb63b1192cd18eeacbb313ff
gauribehl100 <gauribehl02@gmail.com>
2024-06-25 22:38:40 +0530

    Top courses

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
-import "./App.css";
+// import React, { useEffect } from "react";
+// import Navbar from "../react-tailwind-css-starter-pack/src/Shopp/Navbar"
+// import Filter from "../react-tailwind-css-starter-pack/src/Shopp/Filter"
+// import Cards from "../react-tailwind-css-starter-pack/src/Shopp/Cards"
+// import {apiUrl, filterData, /*fit*/}  from "../react-tailwind-css-starter-pack/src/Shopp/Data";
+// import {/*ToastContainer*/toast} from 'react-toastify';
+// import { useState } from "react";
 
-function App() {
-  return (
-    <main className="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 className="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p className="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" className="mx-auto" />
-    </main>
-  );
-}
+// const App = () => {
+//   const [courses, setCourses] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   // const [category, setCategory] = useState(filterData[0].title) 
 
-export default App;
+//   async function fetchData(){
+//     setLoading(true);
+//     try{
+//       let response = await fetch(apiUrl);
+//       let output = await response.json();
+//       setCourses(output.data);
+//     }
+//     catch(error) {
+//       toast.error("Network has no issues"); 
+//     }
+//     setLoading(false);
+//   }
+//   useEffect(() => {
+//     fetchData();
+//   },[])
+
+//   return (
+//   <div className="min-h-screen flex flex-col">
+//     <div>
+//       <Navbar/>
+//     </div>
+//     <div className="bg-[#002B5B]">
+//       <div>
+//         <Filter
+//           filterData = {filterData}
+//           // category={category} 
+//           // setCategory={setCategory} 
+//         />
+//       </div>
+//       <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center item-center min-h[50vh]">
+//         {
+//           loading?(<spinner/>) : (<Cards courses = {courses} /* category={category} *//>)
+//         }
+//       </div>
+//     </div>
+//   </div>
+// )};
+
+// export default App;
+
